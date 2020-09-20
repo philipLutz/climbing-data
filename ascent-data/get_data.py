43bece1a5be3c67436e398c2a4f3d92bdd119164
Philip Lutz <philiplutz413@gmail.com>
2020-09-19 18:10:30 -0700

    working on HTML to CSV script

--- ascent-data/get_data.py
+++ ascent-data/get_data.py
@@ -5,18 +5,52 @@ We are going to parse the raw HTML from 8a.nu, and then convert the logged
 ascents into CSV files. We are separating routes and boulders.
 """
 
+import csv
 from bs4 import BeautifulSoup
 
-# Parse saved HTML file
-soup = BeautifulSoup(
-    open("/Users/philipandrewlutz/climbing-data/ascent-data/boulders.html"),
-    "html.parser")
+file_paths = [
+    "/Users/philipandrewlutz/climbing-data/ascent-data/boulders.html",
+    #"/Users/philipandrewlutz/climbing-data/ascent-data/routes.html"
+]
 
-ascent_table = soup.find("table", "user-ascents")
+def parse_file(path):
+    """
+    Creates CSV file of ascents from 8a.nu HTML
+    """
+    reference_string = path[50:]
+    reference_string = reference_string.split('.')[0]
+    csv_path = ("/Users/philipandrewlutz/climbing-data/ascent-data/ascents_" 
+        + reference_string + ".csv")
+    
+    soup = BeautifulSoup(open(path),"html.parser")
+    ascent_table = soup.find("table", "user-ascents")
 
-headers = []
-for header in ascent_table.thead.find_all("th"):
-    if header.text:
-        headers.append(header.text.strip())
+    #headers = []
+    # for header in ascent_table.thead.find_all("th"):
+    #     if header.text:
+    #         headers.append(header.text.strip())
+    
+    # create dictionaries for each ascent
+    
+    
+    
+    # Write to CSV file
+    with open(csv_path, 'w', newline='') as csvfile:
+        fieldnames = ["name", "crag", "date", "grade", "comment"]
+        writer = csv.DictWriter(csvfile, delimiter=',', fieldnames=fieldnames)
 
-print(headers)
+        writer.writeheader()
+        writer.writerow({
+            "name": "Mandala",
+            "crag": "Bishop",
+            "date": "11/8/2021",
+            "grade": "8A+",
+            "comment": "Sick!"
+        })
+        
+    print("Finished write to:")
+    print("    " + csv_path + "\n")
+    
+
+for path in file_paths:
+    parse_file(path)
